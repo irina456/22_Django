3c7b07826a06c53743c60a336e0052cf043dd675
irina456 <iradyachenko27@gmail.com>
2025-07-11 10:52:37 +0300

    clean rep

--- catalog/media/README.md
+++
@@ -1,1 +0,0 @@
-# The dirrectory of photos
--- catalog/views.py
+++ catalog/views.py
@@ -2,16 +2,6 @@ import logging
 
 from catalog.apps import CatalogProjectConfig
 
-logger_views = logging.getLogger(__name__)
-file_handler = logging.FileHandler(f"log/{__name__}.log", mode="w", encoding="UTF8")
-file_formatter = logging.Formatter(
-    "\n%(asctime)s %(levelname)s %(name)s \n%(funcName)s %(lineno)d: \n%(message)s",
-    datefmt="%H:%M:%S %d-%m-%Y",
-)
-file_handler.setFormatter(file_formatter)
-logger_views.addHandler(file_handler)
-logger_views.setLevel(logging.INFO)
-
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -19,12 +9,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    logger_views.debug(request)
     return render(request, f"{CatalogProjectConfig.name}/home.html")
 
 
 def contacts(request):
-    logger_views.debug(request)
     if request.method == "POST":
         name = request.POST.get("name")
         phone = request.POST.get("phone")
